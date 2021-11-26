c222b24253a7d30352a3934b73f1728b74338f52
Bruno Andreo <brunolino2026@gmail.com>
2021-11-26 20:52:09 -0300

    Added reload conf

--- entrypoint.py
+++ entrypoint.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "entrypoint:app", 
         port=settings.app.port, 
-        log_level=settings.app.log_level)
+        log_level=settings.app.log_level,
+        reload=settings.app.reload)
