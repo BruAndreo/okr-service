caa85f58987840b3a3b2373f91b5db772cc6b5e5
Bruno Andreo <brunolino2026@gmail.com>
2021-12-01 21:31:17 -0300

    renaming env var to database name

--- okr/data/db.py
+++ okr/data/db.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 from okr.config import settings
 
 client = MongoClient(settings.database.host, settings.database.port)
-db = client[settings.database.collection]
+db = client[settings.database.db_name]
