1338abd85070fd003d7fe94aa2f8dc2f512543fe
Bruno Andreo <brunolino2026@gmail.com>
2021-12-27 21:12:41 -0300

    fixing error message in authentication

--- okr/resources/routes/authentication.py
+++ okr/resources/routes/authentication.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from okr.resources.schemas.auth import AuthBody
 from okr.domain.services.user import User
 
@@ -9,7 +9,11 @@ router = APIRouter(
 )
 
 @router.post("/", summary="User Authentication")
-async def auth(auth: AuthBody):
-    user = User()
-    auth_token = user.authenticate_user(auth)
-    return { "token": auth_token }
+async def auth(auth: AuthBody, response: Response):
+    try:
+        user = User()
+        auth_token = user.authenticate_user(auth)
+        return { "token": auth_token }
+    except:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return { "message": "Invalid User or Password" }
