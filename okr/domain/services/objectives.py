f5fe80e2c0c87b6786fa2a75d2eb0ead417bc0d8
Bruno Andreo <brunolino2026@gmail.com>
2021-12-27 22:42:01 -0300

    infra: using uuid generator default for generate all ids

--- okr/domain/services/objectives.py
+++ okr/domain/services/objectives.py
@@ -1,19 +1,15 @@
 from okr.domain.repositories.objective_repository import ObjectiveRepository
 from okr.resources.schemas.objectives import NewObjective
 from okr.domain.models.objectives import Objectives as ObjectivesModel
-
-import uuid
+from okr.utils.uuid import uuid_generator
 
 class Objectives:
     
     def create_new_objetive(self, objective: NewObjective, creator: str):
-        new_objective = ObjectivesModel(objective_id=self._generate_user_id(), 
+        new_objective = ObjectivesModel(objective_id=uuid_generator(), 
                                         user_id=creator, 
                                         **objective.dict())
 
         repo = ObjectiveRepository()
         repo.insert_one(new_objective.dict())
         return new_objective.objective_id
-    
-    def _generate_user_id(self) -> str:
-        return str(uuid.uuid4())
--- okr/domain/services/user.py
+++ okr/domain/services/user.py
@@ -1,23 +1,17 @@
 from okr.resources.schemas.auth import AuthBody
 from okr.resources.schemas.user import UserBody
 from okr.domain.models.user import User as UserModel
-from okr.data.db import db
 from okr.utils.jwt import JWT
 from okr.domain.repositories.user_repository import UserRepository
-
-import uuid
-
+from okr.utils.uuid import uuid_generator
 
 class User:
 
     def create_user(self, user_body: UserBody):
-        user = UserModel(**user_body, user_id=self._generate_user_id())
+        user = UserModel(**user_body, user_id=uuid_generator())
         user_repo = UserRepository()
         user_repo.insert_user(user)
         return user.user_id
-
-    def _generate_user_id(self) -> str:
-        return str(uuid.uuid4())
 
     def authenticate_user(self, auth: AuthBody):
         user_repo = UserRepository()
