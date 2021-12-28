3e2e35a37cdd396df4d7c671a614b3ea750cc62e
Bruno Andreo <brunolino2026@gmail.com>
2021-12-27 22:34:11 -0300

    Added setting to objective collection

--- okr/domain/repositories/objetive_repository.py
+++ okr/domain/repositories/objetive_repository.py
@@ -1,8 +1,9 @@
 from okr.data.builder import MongoBuilder
+from okr.config import settings
 
 class ObjectiveRepository(MongoBuilder):
 
-    _collection = "objetives"
+    _collection = settings.database.objective_collection
 
     def __init__(self):
         super().__init__(self._collection)
